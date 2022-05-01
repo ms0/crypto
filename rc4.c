093e389663121762d2803eb263536f49faf933dd
Mike Speciner <ms@alum.mit.edu>
2022-04-30 21:46:27 -0400

    Changed state variable names x,y->i,j
    For consistency with external docs

--- rc4.c
+++ rc4.c
@@ -1,34 +1,35 @@
 /* RC4 description:
    initialize the state array s to the identity permutation, then
    for i from 0 thru 255 and cycling thru the octets of the key in order:
-    swap the ith element with the kth element, where
-     k has been incremented by the current key octet and by the ith element
-   set x and y to 0 (these are updated and used as indices in rc4step)
-   rc4step: increment x, then increase y by the xth element,
-     then swap the xth and yth elements and use their sum as the index of
+    swap the ith element with the jth element, where
+     j has been incremented by the current key octet and by the ith element
+   set i and j to 0 (these are updated and used as indices in rc4step)
+   rc4step: increment i, then increase j by the ith element,
+     then swap the ith and jth elements and use their sum as the index of
      the element returned
    NOTE: all indices are mod 256
 */
 
 #include <stdint.h>
 
-static uint8_t s[256], x, y;    // 258 octets of state information
+static uint8_t s[256], i, j;    // 258 octets of state information
 
 void rc4init (uint8_t *key, uint16_t length) {    // initialize state
-  uint8_t t, i = 0, k = 0;
+  uint8_t t;
+  i = j = 0;
   do s[i]=i; while (++i);
-  do t = s[i], s[i] = s[k += key[i%length] + t], s[k] = t; while (++i);
-  x = y = 0;
+  do t = s[i], s[i] = s[j += key[i%length] + t], s[j] = t; while (++i);
+  j = 0;
 }
 
 uint8_t rc4step () {    // return next pseudo-random octet
   uint8_t t, u;
-  t = s[y += s[++x]], s[y] = u = s[x], s[x] = t;
+  t = s[j += s[++i]], s[j] = u = s[i], s[i] = t;
   return (s[t += u]);
 }
 
 uint8_t rc4back () {    // step back, return last pseudo-random octet
   uint8_t t, u, v;
-  t = s[x], u = s[y], v = s[v = t + u], s[x--] = u, s[y] = t, y -= u;
+  t = s[i], u = s[j], v = s[v = t + u], s[i--] = u, s[j] = t, j -= u;
   return v;
 }
--- rc4.py
+++ rc4.py
@@ -21,7 +21,7 @@ class rc4(object) :
       state[k] = t;
       j = (j+1)%l;
     self._state = state;
-    self._x = self._y = 0;
+    self._i = self._j = 0;
 
   def __iter__(self) :
     """Return RC4 interator"""
@@ -29,21 +29,21 @@ class rc4(object) :
 
   def __next__(self) :
     """Return next octet in RC4 stream"""
-    self._x = (self._x + 1) & 0xff;
-    self._y = (self._y + self._state[self._x]) & 0xff;
-    self._state[self._x], self._state[self._y] = \
-      self._state[self._y], self._state[self._x];
-    return self._state[(self._state[self._x] + self._state[self._y]) & 0xff]
+    self._i = (self._i + 1) & 0xff;
+    self._j = (self._j + self._state[self._i]) & 0xff;
+    self._state[self._i], self._state[self._j] = \
+      self._state[self._j], self._state[self._i];
+    return self._state[(self._state[self._i] + self._state[self._j]) & 0xff]
     
   def __prev__(self) :
     """Return previous octet in RC4 stream"""
-    t = self._state[self._x];
-    u = self._state[self._y];
+    t = self._state[self._i];
+    u = self._state[self._j];
     v = self._state[(t + u) & 0xff];
-    self._state[self._x] = u;
-    self._state[self._y] = t;
-    self._x = (self._x - 1) & 0xff;
-    self._y = (self._y - u) & 0xff;
+    self._state[self._i] = u;
+    self._state[self._j] = t;
+    self._i = (self._i - 1) & 0xff;
+    self._j = (self._j - u) & 0xff;
     return v;
 
   next = __next__
